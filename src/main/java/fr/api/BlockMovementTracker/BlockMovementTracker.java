867d24664be9232467d72881756a620ae8695f1a
JS <131463436+Julien-Sbth@users.noreply.github.com>
2024-04-10 12:36:01 +0200

    [Update ADD Column Player_Name]

--- src/main/java/fr/api/BlockMovementTracker/BlockMovementTracker.java
+++ src/main/java/fr/api/BlockMovementTracker/BlockMovementTracker.java
@@ -1,6 +1,7 @@
 package fr.api.BlockMovementTracker;
 
 import fr.api.basededonnees.SQLiteManager;
+import net.md_5.bungee.api.chat.hover.content.Text;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -34,7 +35,7 @@ public class BlockMovementTracker implements Listener {
     public void onPlayerMove(PlayerMoveEvent event) {
         Player player = event.getPlayer();
         UUID playerId = player.getUniqueId();
-
+        String playerName = player.getName();
         if (event.getFrom().getBlockX() != event.getTo().getBlockX() ||
                 event.getFrom().getBlockY() != event.getTo().getBlockY() ||
                 event.getFrom().getBlockZ() != event.getTo().getBlockZ()) {
@@ -53,12 +54,20 @@ public class BlockMovementTracker implements Listener {
         for (Map.Entry<UUID, Integer> entry : blocksMovedMap.entrySet()) {
             UUID playerId = entry.getKey();
             int blocksMoved = entry.getValue();
-            savePlayerMovementToDatabase(playerId, blocksMoved);
+
+            Player player = Bukkit.getPlayer(playerId);
+            if (player != null) {
+                String playerName = player.getName(); // Obtenez le nom du joueur à partir de l'objet Player
+                savePlayerMovementToDatabase(playerId, blocksMoved, playerName);
+            } else {
+                Bukkit.getLogger().warning("Impossible de trouver le joueur correspondant à l'UUID: " + playerId);
+            }
         }
         blocksMovedMap.clear();
     }
 
-    private void savePlayerMovementToDatabase(UUID playerId, int blocksMoved) {
+
+    private void savePlayerMovementToDatabase(UUID playerId, int blocksMoved, String playerName) {
         Connection connection = null;
         PreparedStatement statement = null;
         PreparedStatement insertStatement = null;
@@ -72,11 +81,12 @@ public class BlockMovementTracker implements Listener {
                 return;
             }
 
-            statement = connection.prepareStatement("SELECT * FROM player_movement WHERE player_id = ?");
-            insertStatement = connection.prepareStatement("INSERT INTO player_movement (player_id, blocks_moved) VALUES (?, ?)");
-            updateStatement = connection.prepareStatement("UPDATE player_movement SET blocks_moved = blocks_moved + ? WHERE player_id = ?");
+            statement = connection.prepareStatement("SELECT * FROM player_movement WHERE player_id = ? AND player_name = ?");
+            insertStatement = connection.prepareStatement("INSERT INTO player_movement (player_id, blocks_moved, player_name) VALUES (?, ?, ?)");
+            updateStatement = connection.prepareStatement("UPDATE player_movement SET blocks_moved = blocks_moved + ? WHERE player_id = ? AND player_name = ?");
 
             statement.setString(1, playerId.toString());
+            statement.setString(2, playerName); // Utiliser playerName directement
             resultSet = statement.executeQuery();
 
             boolean playerExists = resultSet.next();
@@ -84,10 +94,12 @@ public class BlockMovementTracker implements Listener {
             if (playerExists) {
                 updateStatement.setInt(1, blocksMoved);
                 updateStatement.setString(2, playerId.toString());
+                updateStatement.setString(3, playerName); // Utiliser playerName directement
                 updateStatement.executeUpdate();
             } else {
                 insertStatement.setString(1, playerId.toString());
                 insertStatement.setInt(2, blocksMoved);
+                insertStatement.setString(3, playerName); // Utiliser playerName directement
                 insertStatement.executeUpdate();
             }
         } catch (SQLException e) {
