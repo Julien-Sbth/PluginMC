6b76f393d0f38ea63d030e9188c240e255e3f99a
JS <131463436+Julien-Sbth@users.noreply.github.com>
2024-04-14 18:09:04 +0200

    [Update Main, Add Links For HandleMenu]

--- main.go
+++ main.go
@@ -8,13 +8,14 @@ import (
 )
 
 func main() {
-	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("templates/css"))))
+	http.Handle("/templates/", http.StripPrefix("/templates/", http.FileServer(http.Dir("templates/"))))
 
 	http.HandleFunc("/api", Menu.ApiHandler)
 	http.HandleFunc("/api/user", Menu.UserHandler)
 	http.HandleFunc("/data", Menu.ZombieKillsHandler)
+	http.HandleFunc("/api/player/search", Menu.PlayerInfoHandler)
+	http.HandleFunc("/", Menu.HandleParametre)
 
-	// Démarrer le serveur
 	log.Println("Serveur Go démarré sur le port 8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("Erreur lors du démarrage du serveur:", err)
