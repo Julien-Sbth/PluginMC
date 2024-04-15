66ab94ad88f8835537884a8334e170a8cd37a3f2
JS <131463436+Julien-Sbth@users.noreply.github.com>
2024-04-15 09:42:19 +0200

    [Update Main ADD links ]#

--- main.go
+++ main.go
@@ -15,6 +15,8 @@ func main() {
 	http.HandleFunc("/data", Menu.ZombieKillsHandler)
 	http.HandleFunc("/login", Menu.HandleLogin)
 	http.HandleFunc("/register", Menu.HandleRegister)
+	http.HandleFunc("/logout", Menu.HandleLogout)
+	http.HandleFunc("/shop", Menu.HandleShop)
 	http.HandleFunc("/api/player/search", Menu.PlayerInfoHandler)
 	http.HandleFunc("/", Menu.HandleMenu)
 
