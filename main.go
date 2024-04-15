815e358c54a891dc600b9df33c3d6cd7bf0ac08d
JS <131463436+Julien-Sbth@users.noreply.github.com>
2024-04-15 18:59:41 +0200

    [Update Main]

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"APIMC/API"
+	"APIMC/Connexion"
 	"APIMC/Menu"
 	_ "APIMC/Menu"
 	"log"
@@ -10,16 +12,16 @@ import (
 func main() {
 	http.Handle("/templates/", http.StripPrefix("/templates/", http.FileServer(http.Dir("templates/"))))
 
-	http.HandleFunc("/api", Menu.ApiHandler)
-	http.HandleFunc("/api/user", Menu.UserHandler)
-	http.HandleFunc("/data", Menu.ZombieKillsHandler)
-	http.HandleFunc("/login", Menu.HandleLogin)
-	http.HandleFunc("/register", Menu.HandleRegister)
+	http.HandleFunc("/api", API.ApiHandler)
+	http.HandleFunc("/api/user", API.UserHandler)
+	http.HandleFunc("/data", API.DataHandlerAPI)
+	http.HandleFunc("/login", Connexion.HandleLogin)
+	http.HandleFunc("/register", Connexion.HandleRegister)
 	http.HandleFunc("/about", Menu.HandleAbout)
 
-	http.HandleFunc("/logout", Menu.HandleLogout)
+	http.HandleFunc("/logout", Connexion.HandleLogout)
 	http.HandleFunc("/shop", Menu.HandleShop)
-	http.HandleFunc("/api/player/search", Menu.PlayerInfoHandler)
+	http.HandleFunc("/api/player/search", API.PlayerInfoHandler)
 	http.HandleFunc("/", Menu.HandleMenu)
 
 	log.Println("Serveur Go démarré sur le port 8080")
