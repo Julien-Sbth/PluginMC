015a08b1b6b65f4a7bc99534fe5930ec81b77338
JS <131463436+Julien-Sbth@users.noreply.github.com>
2024-04-26 21:57:27 +0200

    [ADD Folder Utils, Vault for Each Player]

--- src/main/java/fr/api/commands/Commands.java
+++ src/main/java/fr/api/commands/Commands.java
@@ -1,63 +1,43 @@
 package fr.api.commands;
 
-import fr.api.menu.Menu;
-import fr.api.monster.MonsterDeathListener;
+import fr.api.utils.VaultUtils;
 import org.bukkit.Bukkit;
-import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
-public class Commands implements CommandExecutor {
+import java.util.ArrayList;
+
+public class OpenCommand implements CommandExecutor {
 
     @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
         if (sender instanceof Player) {
-            Player player = (Player) sender;
 
-            if (cmd.getName().equalsIgnoreCase("test")) {
-                player.sendMessage("§4Bravo tu as réussi le test");
-                return true;
-            }
-            if (cmd.getName().equalsIgnoreCase("pute")) {
-                player.sendMessage("§4Bravo tu as réussi le test");
-                return true;
-            }
-            if(cmd.getName().equalsIgnoreCase("alert")) {
+            Player p = (Player) sender;
 
-                if(args.length == 0) {
-                    player.sendMessage("la commande est : /alert <message>");
+            if (args.length > 0) {
+                if (args[0].equalsIgnoreCase("open")) {
+
+                    ArrayList<ItemStack> vaultItems = VaultUtils.getItems(p);
+
+                    Inventory vault = Bukkit.createInventory(p, 54, "Your Personal Vault");
+
+                    vaultItems.stream()
+                            .forEach(itemStack -> vault.addItem(itemStack));
+
+                    p.openInventory(vault);
+
                 }
-
-                if(args.length >= 1) {
-                    StringBuilder bc = new StringBuilder();
-                    for(String part : args) {
-                        bc.append(part + " ");
-                    }
-                    Bukkit.broadcastMessage("[" + player.getName() + "] " + bc.toString());
-                }
-
             }
 
-            if (cmd.getName().equalsIgnoreCase("stats")) {
-                int zombieKills = MonsterDeathListener.getMonsterKills(player, "ZOMBIE");
-                int skeletonKills = MonsterDeathListener.getMonsterKills(player, "SKELETON");
-                int creeperKills = MonsterDeathListener.getMonsterKills(player, "CREEPER");
-                int spiderKills = MonsterDeathListener.getMonsterKills(player, "SPIDER");
-
-                player.sendMessage("Nombre de zombies tués : " + zombieKills);
-                player.sendMessage("Nombre de squelettes tués : " + skeletonKills);
-                player.sendMessage("Nombre de creeper tués : " + creeperKills);
-                player.sendMessage("Nombre de araignées tués : " + spiderKills);
-                return true;
-            }
-            if (cmd.getName().equalsIgnoreCase("shop")) {
-                Menu.openCustomMenu(player);
-                return true;
-            }
         }
-        return false;
+
+
+        return true;
     }
 }
--- src/main/java/fr/api/pluginmc/Main.java
+++ src/main/java/fr/api/pluginmc/Main.java
@@ -1,136 +1,32 @@
 package fr.api.pluginmc;
 
-import fr.api.Block.BlockDestroyListener;
-import fr.api.JoinLeave.JoinLeaveListener;
-import fr.api.menu.Menu;
-import fr.api.BlockMovementTracker.BlockMovementTracker;
-import fr.api.advancement.AchievementListener;
-import fr.api.api.APIClient;
-import fr.api.menu.PlayerCoinsManager;
-import fr.api.InventoryManager.InventoryListener;
-import fr.api.artefact.ArtefactItemsListener;
-import fr.api.basededonnees.SQLiteManager;
-import fr.api.commands.Commands;
-import fr.api.menu.ShopManager;
-import fr.api.moneycommands.MoneyCommand;
-import fr.api.monster.MonsterDeathListener;
-import org.bukkit.Material;
-import org.bukkit.event.Listener;
+import fr.api.Listener.Listeners;
+import fr.api.commands.OpenCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.*;
-import java.util.logging.Logger;
+public final class VaultPlugin extends JavaPlugin {
 
-import static fr.api.menu.Menu.getPrice;
-
-public class Main extends JavaPlugin implements Listener {
-    private SQLiteManager sqliteManager;
-    private ShopManager shopManager;
-    private MonsterDeathListener monsterDeathListener;
-    private APIClient apiClient;
-    private PlayerCoinsManager playerCoinsManager;
-    private Menu menu;
+    private static VaultPlugin plugin;
 
     @Override
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(this, this);
-        getCommand("money").setExecutor(new MoneyCommand(monsterDeathListener));
-        getServer().getPluginManager().registerEvents(new ArtefactItemsListener(), this);
+        // Plugin startup logic
 
-        getCommand("test").setExecutor(new Commands(monsterDeathListener));
-        getCommand("alert").setExecutor(new Commands(monsterDeathListener));
-        getCommand("stats").setExecutor(new Commands(monsterDeathListener));
-        getCommand("shop").setExecutor(new Commands(monsterDeathListener));
-        getCommand("sb").setExecutor(new Commands(monsterDeathListener));
+        plugin = this;
 
-        Logger logger = Logger.getLogger("MenuLogger");
+        getCommand("vault").setExecutor(new OpenCommand());
 
-        sqliteManager = new SQLiteManager("database.sqlite");
-        if (sqliteManager.isConnected()) {
-            getLogger().info("Connexion à la base de données établie avec succès !");
-        } else {
-            getLogger().warning("La connexion à la base de données a échoué !");
-        }
-        menu = new Menu(this, sqliteManager, logger);
+        getServer().getPluginManager().registerEvents(new Listeners(), this);
 
-        getServer().getPluginManager().registerEvents(new JoinLeaveListener(), this);
-
-        menu.registerEvents();
-        Listener inventoryListener = new InventoryListener(sqliteManager, getLogger());
-
-        getServer().getPluginManager().registerEvents(inventoryListener, this);
-        apiClient = new APIClient(sqliteManager);
-        getServer().getPluginManager().registerEvents(new BlockDestroyListener(sqliteManager), this);
-        List<Material> displayedItems = Arrays.asList(
-                Material.NETHERITE_SWORD, Material.NETHERITE_BOOTS, Material.DIAMOND_SWORD,
-                Material.GOLDEN_BOOTS, Material.LEATHER_BOOTS, Material.WOODEN_SWORD,
-                Material.IRON_SWORD, Material.GOLDEN_SWORD, Material.NETHERITE_LEGGINGS,
-                Material.NETHERITE_CHESTPLATE, Material.LEATHER_LEGGINGS, Material.WOODEN_SWORD,
-                Material.GOLDEN_SWORD, Material.NETHERITE_HELMET,
-                Material.IRON_HELMET, Material.GOLDEN_HELMET, Material.DIAMOND_HELMET,
-                Material.LEATHER_HELMET, Material.NETHERITE_HELMET, Material.LEATHER_LEGGINGS,
-                Material.NETHERITE_PICKAXE, Material.DIAMOND_PICKAXE, Material.DIAMOND_AXE,
-                Material.NETHERITE_AXE, Material.IRON_AXE, Material.GOLDEN_AXE,
-                Material.WOODEN_AXE, Material.STONE_AXE, Material.BOW,
-                Material.IRON_BOOTS, Material.DIAMOND_BOOTS, Material.DIAMOND_CHESTPLATE,
-                Material.GOLDEN_CHESTPLATE, Material.LEATHER_CHESTPLATE, Material.IRON_CHESTPLATE,
-                Material.DIAMOND_LEGGINGS, Material.IRON_LEGGINGS, Material.GOLDEN_LEGGINGS, Material.SHIELD,
-                Material.IRON_PICKAXE, Material.WOODEN_PICKAXE, Material.STONE_PICKAXE, Material.RED_STAINED_GLASS,
-                Material.DIAMOND_PICKAXE,
-                Material.NETHERITE_SWORD,
-                Material.FISHING_ROD,
-                Material.POTION
-        );
-
-        for (Material material : displayedItems) {
-            int price = getPrice(material);
-            if (!isItemAlreadyInShop(material)) {
-                shopManager.addItemToShop(material, 1, price);
-            }
-        }
-
-        ShopManager shopManager = new ShopManager(sqliteManager);
-        apiClient.sendPlayerDataToSite();
-
-        getServer().getPluginManager().registerEvents(new MonsterDeathListener(sqliteManager, getLogger(), List.of("")), this);
-        getServer().getPluginManager().registerEvents(new BlockMovementTracker(sqliteManager, this), this);
-        getServer().getPluginManager().registerEvents(new AchievementListener(sqliteManager), this);
-        playerCoinsManager = new PlayerCoinsManager(this, sqliteManager);
-        playerCoinsManager = new PlayerCoinsManager(this, sqliteManager);
-
-        BlockMovementTracker movementTracker = new BlockMovementTracker(sqliteManager, this);
-        getServer().getPluginManager().registerEvents(movementTracker, this);
-    }
-
-    private boolean isItemAlreadyInShop(Material material) {
-        try {
-            if (!sqliteManager.isConnected()) {
-                throw new SQLException("La connexion à la base de données est fermée.");
-            }
-
-            try (PreparedStatement selectPs = sqliteManager.getConnection().prepareStatement(
-                    "SELECT COUNT(*) AS count FROM Shop WHERE item_name = ?")) {
-                selectPs.setString(1, material.toString());
-                ResultSet resultSet = selectPs.executeQuery();
-                if (resultSet.next()) {
-                    int count = resultSet.getInt("count");
-                    return count > 0;
-                }
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return false;
     }
 
     @Override
     public void onDisable() {
-        if (sqliteManager != null) {
-            sqliteManager.closeConnection();
-        }
-        getLogger().info("Le Plugin MC vient de s'éteindre !");
+        // Plugin shutdown logic
+    }
+
+    public static VaultPlugin getPlugin() {
+        return plugin;
     }
 }
+
