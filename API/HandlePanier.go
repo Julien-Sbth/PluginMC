46d5f989dd47bfdb95894c1c3dd92b2e45c5921b
JS <131463436+Julien-Sbth@users.noreply.github.com>
2024-05-02 10:10:04 +0200

    [Upgrade Files ADD Coins & Connected]

--- API/HandlePanier.go
+++ API/HandlePanier.go
@@ -17,7 +17,6 @@ func HandleAddToCart(w http.ResponseWriter, r *http.Request) {
 	itemID := r.URL.Query().Get("itemID")
 	addToCart(itemID)
 
-	// Rediriger vers la page de panier ou de connexion selon l'état de connexion de l'utilisateur
 	session, err := Connexion.Store.Get(r, Connexion.SessionName)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -65,22 +64,58 @@ func HandlePanier(w http.ResponseWriter, r *http.Request) {
 			log.Println("L'utilisateur", username, "est un administrateur")
 		}
 
-		// Charger les données de panier
-		// Ici, nous devons utiliser une logique pour charger les données du panier
-		// playerData.Panier = loadPanierData(username)
-
 		tmpl, err := template.ParseFiles("templates/html/Menu/panier.html")
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
+		var coins []Coin
+		var kills []Kill
+		var item []Item
+		var connect []Connected
+		coinsFound := false
 
+		if usernameExists(username) {
+			for _, c := range playerData.Coins {
+				if c.PlayerName == username {
+					coins = append(coins, c)
+					coinsFound = true
+				}
+			}
+			for _, c := range playerData.Connected {
+				if c.Name == username {
+					connect = append(connect, c)
+					coinsFound = true
+				}
+			}
+			for _, k := range playerData.Kills {
+				if k.PlayerID == username {
+					kills = append(kills, k)
+				}
+			}
+			for _, i := range playerData.Items {
+				if i.PlayerID == username {
+					item = append(item, i)
+				}
+			}
+			for _, c := range playerData.Connected {
+				if c.UUID == username {
+					connect = append(connect, c)
+				}
+			}
+		}
+
+		if !coinsFound {
+			coins = append(coins, Coin{PlayerID: username, Coins: "0"})
+		}
 		playerData := PlayerData{
 			Username:     username,
 			IsLoggedIn:   true,
 			IsAdmin:      isAdmin,
-			Panier:       playerData.Panier, // Assurez-vous que playerData.Panier est correctement chargé
+			Panier:       playerData.Panier,
 			ErrorMessage: "Joueur Inconnu, veuillez en choisir un autre",
+			Coins:        coins,
+			Connected:    connect,
 		}
 
 		err = tmpl.Execute(w, playerData)
@@ -91,15 +126,11 @@ func HandlePanier(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Si l'utilisateur n'est pas connecté, rediriger vers la page de connexion
 	http.Redirect(w, r, "/api/user/login", http.StatusSeeOther)
 }
 
 func addToCart(itemID string) {
-	// Fonction factice pour ajouter un article au panier
-	// Vous devrez peut-être implémenter votre propre logique ici pour obtenir les détails de l'article à partir de l'ID
 	var itemToAdd ShopItem
-	// Supposez que playerData soit une variable globale qui stocke les données de l'utilisateur
 	for _, item := range playerData.ShopItem {
 		if item.ID == itemID {
 			itemToAdd = item
