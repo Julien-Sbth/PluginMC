02fa73044bca40553744ebb60688436bb9bf0708
JS <131463436+Julien-Sbth@users.noreply.github.com>
2024-05-02 09:57:49 +0200

    [Update Files ADD Listener Join Left Player Database]

--- src/main/java/fr/api/pluginmc/Main.java
+++ src/main/java/fr/api/pluginmc/Main.java
@@ -2,6 +2,8 @@ package fr.api.pluginmc;
 
 import fr.api.Block.BlockDestroyListener;
 import fr.api.JoinLeave.JoinLeaveListener;
+import fr.api.JoinLeave.PlayerJoinQuitListener;
+import fr.api.JoinLeave.PlayerManager;
 import fr.api.Listener.ListenerVaultPlayer;
 import fr.api.menu.Menu;
 import fr.api.BlockMovementTracker.BlockMovementTracker;
@@ -16,15 +18,18 @@ import fr.api.menu.ShopManager;
 import fr.api.moneycommands.MoneyCommand;
 import fr.api.monster.MonsterDeathListener;
 import fr.api.shop.ShopADDItem;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.JDABuilder;
-import javax.security.auth.login.LoginException;
+
 import java.util.List;
 import java.util.logging.Logger;
 
 public class Main extends JavaPlugin implements Listener {
+    private PlayerManager playerManager;
+
     private SQLiteManager sqliteManager;
     private ShopManager shopManager;
     private MonsterDeathListener monsterDeathListener;
@@ -55,11 +60,11 @@ public class Main extends JavaPlugin implements Listener {
         getConfig().options().copyDefaults();
         saveDefaultConfig();
 
-        getCommand("staffhome").setExecutor(new Commands(monsterDeathListener,this));
+        getCommand("staffhome").setExecutor(new Commands(monsterDeathListener, this));
 
         plugin = this;
         getCommand("vault").setExecutor(new Commands(monsterDeathListener, this));
-        getServer().getPluginManager().registerEvents(new ListenerVaultPlayer(),this);
+        getServer().getPluginManager().registerEvents(new ListenerVaultPlayer(), this);
         sqliteManager = new SQLiteManager("database.sqlite");
         if (sqliteManager.isConnected()) {
             getLogger().info("Connexion à la base de données établie avec succès !");
@@ -89,7 +94,10 @@ public class Main extends JavaPlugin implements Listener {
 
         BlockMovementTracker movementTracker = new BlockMovementTracker(sqliteManager, this);
         getServer().getPluginManager().registerEvents(movementTracker, this);
+        playerManager = new PlayerManager(sqliteManager);
+        getServer().getPluginManager().registerEvents(new PlayerJoinQuitListener(sqliteManager), this);
     }
+
 
     @Override
     public void onDisable() {
@@ -97,5 +105,15 @@ public class Main extends JavaPlugin implements Listener {
             sqliteManager.closeConnection();
         }
         getLogger().info("Le Plugin MC vient de s'éteindre !");
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        playerManager.playerJoined(event.getPlayer());
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        playerManager.playerLeft(event.getPlayer());
     }
 }
